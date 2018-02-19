c13ca97576f7155bdb3237f375dd860201ed282e
wessamzaghloul <wessamzaghloul90@gmail.com>
2018-02-19 17:45:11 +0400

    [ADD] scss and js

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,39 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin"); //installed via npm
 const webpack = require("webpack"); //to access built-in plugins
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
 
 const config = {
-  entry: "./src/js/index.js",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename: "bundle.js"
   },
   module: {
-    rules: [{ test: /\.txt$/, use: "raw-loader" }]
+    rules: [
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+            use: [{
+                loader: "css-loader"
+            }, {
+                loader: "sass-loader"
+            }],
+            // use style-loader in development
+            fallback: "style-loader"
+        })
+    }
+    ]
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin(),
-    new HtmlWebpackPlugin({ template: "./src/index.html" })
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    extractSass
   ],
   devServer: {
     port: 3000,
