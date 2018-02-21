3796c46700db653f5120648a8bf85368bd864712
wessamzaghloul <wessamzaghloul90@gmail.com>
2018-02-21 10:32:21 +0400

    [ADD] rtl

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin"); //installed via npm
+
 const webpack = require("webpack"); //to access built-in plugins
 const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
+const WebpackRTLPlugin = require("webpack-rtl-plugin");
 const extractSass = new ExtractTextPlugin({
-    filename: "[name].[contenthash].css",
-    disable: process.env.NODE_ENV === "development"
+  filename: "style.[contenthash].css",
+  disable: process.env.NODE_ENV === "development"
 });
 
 const config = {
@@ -19,21 +20,34 @@ const config = {
       {
         test: /\.scss$/,
         use: extractSass.extract({
-            use: [{
-                loader: "css-loader"
-            }, {
-                loader: "sass-loader"
-            }],
-            // use style-loader in development
-            fallback: "style-loader"
+          use: [
+            {
+              loader: "css-loader"
+            },
+            {
+              loader: "sass-loader"
+            }
+          ],
+          // use style-loader in development
+          fallback: "style-loader"
         })
-    }
+      }
     ]
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin(),
-    new HtmlWebpackPlugin({ template: "./src/index.html" }),
-    extractSass
+    
+    extractSass,
+    new WebpackRTLPlugin({
+      filename: "style.[contenthash].rtl.css",
+      options: {},
+      plugins: [],
+      diffOnly: false,
+      minify: true
+    }),
+    new HtmlWebpackPlugin({
+      mobile: true,
+    })
   ],
   devServer: {
     port: 3000,
